8e01931d714ac4b7c803f1010948abb55edf620f
cesarmmaia <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>
2025-09-11 15:22:40 -0300

    novas alterações para o render

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,5 @@
 from flask import Flask
 
 def create_app():
-    app = Flask(__name__, template_folder='templates', static_folder='static')
+    app = Flask(__name__, template_folder='app/templates', static_folder='app/static')
     return app
